5f303e76f822eaeee62f2d169ca18601fc5afb63
Elizbeh <eliz01_01@yahoo.com>
2023-05-17 11:26:48 +0000

    Correction pycodestyle

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -7,6 +7,12 @@ from typing import Callable
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiply(numb: float) -> float:
         """
+        A type-annotated function make
+        _multiplier that takes a float
+        multiplier as argument and
+        returns a function tha
+        multiplies a float b
+        multiplier.
         """
         return numb * multiplier
     return multiply
