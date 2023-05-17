762dbe54182a5c2da5885c1ae47dbfed851c55cb
Elizbeh <eliz01_01@yahoo.com>
2023-05-17 07:23:48 +0000

    Correction in ppycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 '''Basic annotation - add'''
 
+
 def add(a: float, b: float) -> float:
-    ''' A type annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
+    ''' A type annotated function add
+    that takes a float a and a float b
+    as arguments and returns their sum
+    as a float.
     '''
     return a + b
