b2ee758b7bc5809f8a37bb5299b910a17bfbc3ce
Elizbeh <eliz01_01@yahoo.com>
2023-05-20 21:12:03 +0000

    1. Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
 
-from typing import List, Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float]:
+async def async_comprehension() -> List[float]:
     """
     The coroutine will collect 10
     random numbers using an async
