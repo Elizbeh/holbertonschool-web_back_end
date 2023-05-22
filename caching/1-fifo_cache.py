a1615932ae7128718059e4824258aaf5df5b0b78
Elizbeh <eliz01_01@yahoo.com>
2023-05-22 08:11:17 +0000

    FIFO caching

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -12,11 +12,15 @@ class FIFOCache(BaseCaching):
     """
     def __init__(self):
         """
+        Calls the parent init
         """
         super().__init__()
 
     def put(self, key, item):
         """
+         Assign to the dictionary
+         self.cache_data the item
+         value for the key key.
         """
         if key is None or item is None:
             return
@@ -26,3 +30,11 @@ class FIFOCache(BaseCaching):
             print("DISCARD:", firstInKey)
 
         self.cache_data[key] = item
+
+    def get(self, key):
+
+        """Return the value
+        in self.cache_data linked to
+        key."""
+        if key is None or key not in self.cache_data:
+            return None
