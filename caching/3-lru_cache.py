be558806da069bf785b3f1278be33bc2eff7d040
Elizbeh <eliz01_01@yahoo.com>
2023-05-22 13:34:50 +0000

    3. LRU Caching

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -8,18 +8,18 @@ class LRUCache(BaseCaching):
     """
     A class LRUCache that inherits
     from BaseCaching
-     """
+    """
     def __init__(self):
         super().__init__()
         self.usage_order = []
 
     def put(self, key, item):
+        """ Update usage order if
+            key already exists"""
         if key is None or item is None:
             return
 
         if key in self.cache_data:
-            """ Update usage order if
-            key already exists"""
             self.usage_order.remove(key)
         elif len(self.cache_data) >= self.MAX_ITEMS:
             """ Discard the least
@@ -32,12 +32,12 @@ class LRUCache(BaseCaching):
         self.usage_order.append(key)
 
     def get(self, key):
+        """ Update usage order when
+        key is accessed"""
         if key is None or key not in self.cache_data:
             return None
 
-        """ Update usage order when
-        key is accessed"""
-        self.usage_order.remove(key)
+            self.usage_order.remove(key)
         self.usage_order.append(key)
 
         return self.cache_data[key]
