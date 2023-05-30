a7182bbbeb9a2580447c291a98062962d2fb3c95
Elizbeh <eliz01_01@yahoo.com>
2023-05-30 17:44:20 +0000

    Test

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,12 +3,10 @@
 Regex-ing
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """
-     Returns the log message
-     obfuscated
-    """
-    pattern = f"({separator}|^)({'|'.join(fields)})=.*?({separator}|$)"
-    return re.sub(pattern, fr"\1\2={redaction}\3", message)
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    regex = r"password=([^;\s]+).*date_of_birth=([^;\s]+)"
+
+    return re.sub(regex, f"password={redaction};date_of_birth={redaction}", message)
