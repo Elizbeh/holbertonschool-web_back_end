8fb63c97617d8082eb165d284ceb31c541abc3ce
Elizbeh <eliz01_01@yahoo.com>
2023-05-31 16:39:37 +0200

    Update file

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,23 +7,35 @@ from logging import StreamHandler
 from typing import Tuple
 
 
-PII_FIELDS = ("name", "email", "phone",
-              "ssn", "password")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+class RedactingFormatter(logging.Formatter):
+    def __init__(self, pii_fields):
+        super().__init__()
+        self.pii_fields = pii_fields
+
+    def format(self, record):
+        for field in self.pii_fields:
+            if field in record.msg:
+                record.msg = record.msg.replace(record.msg, "*" * 8)
+        return super().format(record)
 
 
 def get_logger() -> logging.Logger:
     """
-    A function that  returns a
-    logging.Logger object.
+    A function that returns a logging.Logger object.
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    
+
     stream_handler = StreamHandler()
     stream_handler.setLevel(logging.INFO)
-    
+
     formatter = RedactingFormatter(PII_FIELDS)
     stream_handler.setFormatter(formatter)
+
     logger.addHandler(stream_handler)
+
     return logger
