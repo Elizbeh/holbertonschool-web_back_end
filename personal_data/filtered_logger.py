18065de0755615d52b578c2dab5d00fd597dc8fd
Elizbeh <eliz01_01@yahoo.com>
2023-05-31 14:24:33 +0200

    1. Log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
 """
- Regex-ing
+main
 """
 
 import re
 from typing import List
+import logging
 
 
-def filter_datum(
-  fields: List[str], redaction: str, message: str, separator: str
-  ) -> str:
-    """A function that returns the log
-    message obfuscated:
-    """
-    pattern = f'({"|".join(fields)})=[^;]+'
-    return re.sub(pattern, f'\\1={redaction}', message)
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Method to filter values in incoming log records
+        using filter_dum
+        """
+        log_message = super().format(record)
+        return self.__class__.filter_datum(
+            self.fields, self.REDACTION, log_message, self.SEPARATOR
+            )
+
+    @staticmethod
+    def filter_datum(
+      fields: List[str], redaction: str, message: str, separator: str
+      ) -> str:
+        """
+        """
+        pattern = re.compile(f'({"|".join(fields)})=[^;]+')
+        return pattern.sub(f'\\1={redaction}', message)
