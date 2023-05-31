2223a77ba9823f2476ee6fda6c14c2f7c284ce35
Elizbeh <eliz01_01@yahoo.com>
2023-05-31 13:46:16 +0200

    0. Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,40 +1,17 @@
 #!/usr/bin/env python3
 """
-main
+ Regex-ing
 """
 
 import re
 from typing import List
-import logging
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """
-        Method to filter values in incoming log records
-        using filter_dum
-        """
-        log_message = super().format(record)
-        return self.__class__.filter_datum(
-            self.fields, self.REDACTION, log_message, self.SEPARATOR
-            )
-
-    @staticmethod
-    def filter_datum(
-      fields: List[str], redaction: str, message: str, separator: str
-      ) -> str:
-        """
-        """
-        pattern = re.compile(f'({"|".join(fields)})=[^;]+')
-        return pattern.sub(f'\\1={redaction}', message)
+def filter_datum(
+  fields: list[str], redaction: str, message: str, separator: str
+  ) -> str:
+    """A function that returns the log
+gi    message obfuscated:
+    """
+    regex = re.compile(f'({"|".join(fields)})=[^{separator}]+(?={separator})')
+    return regex.sub(f'\\1={redaction}', message)
