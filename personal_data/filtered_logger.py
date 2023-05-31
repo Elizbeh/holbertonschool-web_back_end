853ea4f58802582f3936f0a5fb6c928b9aa87ddf
Elizbeh <eliz01_01@yahoo.com>
2023-05-31 15:25:36 +0200

    2. Create logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,40 +1,28 @@
 #!/usr/bin/env python3
 """
-main
+Main file
 """
-
-import re
-from typing import List
 import logging
+from logging import StreamHandler
+from typing import Tuple
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+PII_FIELDS = ("name", "email", "phone",
+              "ssn", "password")
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """
-        Method to filter values in incoming log records
-        using filter_dum
-        """
-        log_message = super().format(record)
-        return self.__class__.filter_datum(
-            self.fields, self.REDACTION, log_message, self.SEPARATOR
-            )
-
-    @staticmethod
-    def filter_datum(
-      fields: List[str], redaction: str, message: str, separator: str
-      ) -> str:
-        """
-        """
-        pattern = re.compile(f'({"|".join(fields)})=[^;]+')
-        return pattern.sub(f'\\1={redaction}', message)
+def get_logger() -> logging.Logger:
+    """
+    A function that  returns a
+    logging.Logger object.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler = StreamHandler()
+    stream_handler.setFormatter(formatter)
+    
+    logger.addHandler(stream_handler)
+    return logger
