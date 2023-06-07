4302113f068b0f4340e60752bc05f1acbd823a24
Elizbeh <eliz01_01@yahoo.com>
2023-06-07 09:20:49 +0000

    Test

--- user_authentication_service/main.py
+++ user_authentication_service/main.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python3
-"""
-Main file
-"""
-
 from db import DB
 from user import User
 
 my_db = DB()
 
 user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
+print("DB.add_user returns a user object:", isinstance(user_1, User) and user_1.email == "test@test.com" and user_1.hashed_password == "SuperHashedPwd")
 
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
+print("DB.add_user returns a user object:", isinstance(user_2, User) and user_2.email == "test1@test.com" and user_2.hashed_password == "SuperHashedPwd1")
+
