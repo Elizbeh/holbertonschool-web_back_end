a4f164bae49dbd3b70af5f2eb3a150cca80fddee
Elizbeh <eliz01_01@yahoo.com>
2023-06-07 11:15:16 +0000

    Test

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -7,6 +7,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 class DB:
