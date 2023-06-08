4e5e08fdde5f06e796ecdebe1463c30d91fd9ee9
Elizbeh <eliz01_01@yahoo.com>
2023-06-08 12:13:49 +0000

    ...

--- user_authentication_service/main.py
+++ user_authentication_service/main.py
@@ -10,8 +10,6 @@ my_db = DB()
 
 user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
 print(user_1.id)
-assert isinstance(user_1, User)
 
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
 print(user_2.id)
-assert isinstance(user_2, User)
