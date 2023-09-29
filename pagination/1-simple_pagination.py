b9f15f8bf071591ff95a1a2b900c400bbc5570a7
Elizbeh <eliz01_01@yahoo.com>
2023-09-29 10:10:34 +0200

    Pycodestyle correction

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -36,8 +36,8 @@ class Server:
 
         assert isinstance(page, 
                           int) and page > 0, "Page must be a positive integer."
-        assert isinstance(page_size,
-                          int)and page_size > 0, "Page size must be a positive integer."
+        assert isinstance(page_size, 
+                          int) and page_size > 0, "Page size must be a positive int."
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
