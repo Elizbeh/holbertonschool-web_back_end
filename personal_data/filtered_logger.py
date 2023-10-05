502fbde9ead19bf7168d7eb141a16969aa4e30ae
root <root@LAPTOP-J7HTNKBB>
2023-10-05 09:20:10 +0200

    0 Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
+"""
+Write a function called filter_datum that
+returns the log message obfuscated:
+"""
+
 import re
 from typing import List
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
