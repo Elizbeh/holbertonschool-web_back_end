065953a5eef9b2b364bf157be6804fa0651d373e
root <root@LAPTOP-J7HTNKBB>
2023-10-05 09:46:55 +0200

    1. Log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,28 @@ returns the log message obfuscated:
 
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_message = super(
+                RedactingFormatter, self).format(record)
+        for field in self.fields:
+            log_message = filter_datum(
+                    [field], self.REDACTION, log_message, self.SEPARATOR)
+        return log_message
 
 
 def filter_datum(fields: List[str], redaction: str,
