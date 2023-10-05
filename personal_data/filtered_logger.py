3c1bc05209efda74915c725e01b50c8da648df83
root <root@LAPTOP-J7HTNKBB>
2023-10-05 09:29:56 +0200

    Pycodestyle correction

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -14,20 +14,24 @@ def filter_datum(fields: List[str], redaction: str,
     Obfuscate sensitive information in a log message.
 
     Args:
-    - fields (List[str]): A list of strings representing fields to be obfuscated.
-    - redaction (str): A string representing the value by which the fields will be obfuscated.
-    - message (str): A string representing the log line that needs processing.
-    - separator (str): A string representing the character by which fields are separated in the log line.
+    - fields (List[str]): A list of strings
+    representing fields to be obfuscated.
+    - redaction (str): A string representing
+    the value by which the fields will be obfuscated.
+    - message (str): A string representing
+    the log line that needs processing.
+    - separator (str): A string representing
+    the character by which fields are separated in the log line.
 
     Returns:
     - str: The log message with specified fields obfuscated
     """
     # Create a regex pattern by joining escaped field names
     pattern = '|'.join(map(re.escape, fields))
-    
+
     # Use re.sub to perform the substitution with a single regex
     return re.sub(
-        r'({})([^{}]*(?:{}|$))'.format(pattern, re.escape(separator), re.escape(separator)), 
-        r'\1={}{}'.format(redaction, separator), 
-        message
+            r'({})([^{}]*(?:{}|$))'.format(
+                pattern, re.escape(separator), re.escape(separator)),
+            r'\1={}{}'.format(redaction, separator), message
     )
