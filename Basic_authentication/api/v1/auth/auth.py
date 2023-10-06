36e7780387658a5a5a125a11a49f8247157229c7
root <root@LAPTOP-J7HTNKBB>
2023-10-06 13:13:43 +0200

    3. Auth class

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,6 +7,8 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """ A class to manage the API authentication.
+    """
     def require_auth(self, path: str,
                      excluded_paths: List[str]) -> bool:
         """
