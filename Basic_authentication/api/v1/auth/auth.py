7a604f6c62d123acecf3e4d9a2c4c3a38dbe9ccc
root <root@LAPTOP-J7HTNKBB>
2023-10-06 13:07:11 +0200

    3. Auth class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Class
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str,
+                     excluded_paths: List[str]) -> bool:
+        """
+        Placeholder for authentication logic.
+
+        path: The path of the request.
+        excluded_paths: List of paths
+        that should be excluded from authentication.
+        :return: False for now.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Placeholder for obtaining the authorization header.
+
+        :param request: Flask request object.
+        :return: None for now.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Placeholder for obtaining the current user.
+
+        :param request: Flask request object.
+        :return: None for now.
+        """
+        return None
