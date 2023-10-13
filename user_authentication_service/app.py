46df66044f60637b1bc2c8bd4e784fed9cceb8fa
root <root@LAPTOP-J7HTNKBB>
2023-10-13 15:42:59 +0200

    7. Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,9 +2,13 @@
 """
 Basic Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -12,6 +16,22 @@ def Bienvenue():
     """
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """
+    """
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = AUTH.register_user(email, password)
+
+        return jsonify({"email": user.email, "message": "user created"}), 200
+
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 
 if __name__ == "__main__":
