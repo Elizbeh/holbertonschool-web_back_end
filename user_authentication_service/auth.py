26550f67b05e780d6cef3e8435573984e32af1fe
root <root@LAPTOP-J7HTNKBB>
2023-10-15 21:17:30 +0200

    ......

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -77,15 +77,15 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
-        """
-        Get the user corresponding to a session ID.
-        """
+    def get_user_from_session_id(self, session_id):
+        ''' takes a single session_id string argument and returns
+        the corresponding User or None.
+        '''
         if session_id is None:
             return None
-
         try:
-            user = self._db.find_user_by_session_id(session_id)
+            user = self._db.find_user_by(session_id=session_id)
             return user
         except NoResultFound:
             return None
+
