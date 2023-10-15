8b58121ff49842a78e4da3e5e91bd3abde2c2e80
root <root@LAPTOP-J7HTNKBB>
2023-10-15 20:32:51 +0200

    11. Log in

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -34,5 +34,24 @@ def register_user():
         return jsonify({"message": str(e)}), 400
 
 
+@app.route("/sessions")
+def login():
+    """
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(401)
+
+    session_id = auth.create_session(email)
+
+    response_data = {"email": email, "message": "logged in"}
+    response = make_response(jsonify(response_data), 200)
+
+    response.set_cookie('session_id', session_id)
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
