41bbb0aecd5adf9e0bbccbff6558cfbe648cd47f
root <root@LAPTOP-J7HTNKBB>
2023-10-17 10:12:54 +0200

    0. Basic Flask app

---
+++ i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    title = "Welcome to Holberton"
+    h1 = "Hello world"
+    return render_template('0-index.html', title=title, h1=h1)
+
