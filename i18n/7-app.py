86fe48da9ee75d4f6e5d25ef5d5c70e32386ac47
root <root@LAPTOP-J7HTNKBB>
2023-10-22 22:52:14 +0200

    7. Infer appropriate time zone

---
+++ i18n/7-app.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+"""A basic Flask application with user data and login
+simulation.
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+# Mock user table
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(user_id):
+    """
+    Retrieve user information based on user ID.
+
+    Args:
+        user_id (int): The ID of the user to retrieve.
+
+    Returns:
+        dict: A dictionary containing user information,
+        or None if the user is not found.
+    """
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """
+    Process and set user information before handling the request.
+
+    If the 'login_as' parameter is provided in the request,
+    it retrieves the user based on the user ID and stores
+    the user information in the 'g.user' variable
+    for the current request.
+    If 'login_as' is not provided, 'g.user' is set to None.
+    """
+    user_id = request.args.get('login_as')
+    g.user = get_user(int(user_id)) if user_id else None
+
+
+@app.route('/')
+def welcome():
+    """
+    Render a welcome message based on the user's login status.
+    """
+    return render_template('5-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    '''
+    try:
+        locale = request.args.get('locale')
+        if locale in Config.LANGUAGES:
+            return locale
+        locale = g.user.get('locale')
+        if locale in Config.LANGUAGES:
+            return locale
+    except Exception:
+        pass
+    return request.accept_languages.best_match('LANGUAGES')
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """
+    Determine the best-matching time zone for the current request.
+    Prioritizes URL parameters, user settings, and defaults to app config.
+    Returns the selected time zone.
+    """
+    try:
+        url_timezone = request.args.get('timezone')
+        if url_timezone and pytz.timezone(url_timezone):
+            return url_timezone
+
+        user_timezone = g.user.get('timezone')
+        if user_timezone and pytz.timezone(user_timezone):
+            return user_timezone
+    except pytz.exceptions.UnknownTimeZoneError:
+        pass  # Handle invalid time zone
+
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
