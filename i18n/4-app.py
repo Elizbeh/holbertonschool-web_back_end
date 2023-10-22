b4d1bcd28d1d98931efe4ac78add906efa20a753
root <root@LAPTOP-J7HTNKBB>
2023-10-22 18:08:23 +0200

    5. Mock logging in

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python3
+
 """Setup a basic Flask app
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
-
+from flask_babel import Babel
 
 app = Flask(__name__)
-
+babel = Babel(app)
 
 class Config:
     """
-    A class that Config available laguages in the Babel-app
+    A class that Config available languages in the Babel-app
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
 app.config.from_object(Config)
 
-babel = Babel(app)
-
+@app.route('/')
+def welcome():
+    """A basic Flask app that creates a single /
+    and an HTML template...
+    """
+    return render_template('4-index.html')
 
 @babel.localeselector
 def get_locale():
     """
+    Determine the best-matching language from the supported languages.
 
+    Uses request.accept_languages to determine the best match.
     """
     if 'locale' in request.args:
         locale = request.args['locale']
@@ -35,16 +40,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 babel.init_app(app)
-babel.localeselector(get_locale)
-
-
-@app.route('/')
-def welcome():
-    """A basic Flask app that creates a single /
-    and an html template...
-    """
-    return render_template('2-index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
+
