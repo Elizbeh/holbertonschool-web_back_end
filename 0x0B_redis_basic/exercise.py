bebca1640af803ab55cefdfba20525deef495ea1
root <root@LAPTOP-J7HTNKBB>
2023-10-30 16:26:32 +0100

    1. Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,7 +5,7 @@ Writing strings to Redis
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -34,3 +34,55 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None
+            ) -> Union[str, bytes, int, float]:
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(elf, key: str) -> str:
+        """
+        Retrieves a string from Redis using the provided key.
+
+        Args:
+            key (str): The key under which the data is stored in Redis.
+
+        Returns:
+            str: The retrieved data as a string.
+        """"""
+        Retrieves a string from Redis using the provided key.
+
+        Args:
+            key (str): The key under which the data is stored in Redis.
+
+        Returns:
+            str: The retrieved data as a string.
+        """
+        return self.get(fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieves an integer from Redis using the provided key.
+
+        Args:
+            key (str): The key under which the data is stored in Redis.
+
+        Returns:
+            int: The retrieved data as an integer.
+        """
+        return self.get(fn)
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
