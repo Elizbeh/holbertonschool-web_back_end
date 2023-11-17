aed30cf94d597dacdbe565c7e223db8855c8b6ec
root <root@LAPTOP-J7HTNKBB>
2023-11-17 11:44:42 +0100

    First commit

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,13 @@
 function getResponseFromAPI(){
-  return new Promise((resolve, reject) =>{
-    const responseApi = {data: "Data fetched"}
-    if (responseApi) {
-      resolve(responseApi)
-    }else{
-      reject(new Error("Processing Failed"))
-    }
-  })
+  return new Promise((resolve, reject) => {
+    //Simulating an asyncronous API call
+    setTimeout(() => {
+      const responseApi = {data:"Data retrival successful"};
+      if (responseApi) {
+        resolve(responseApi);
+      }else{
+        reject(new Error("Failed to retrieve data"));
+      }
+    }, 1000); // 1 second delay
+  });
 }
