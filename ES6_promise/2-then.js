e901f3c0abda4a6f87b41a75afb566f1a0aa67a2
root <root@LAPTOP-J7HTNKBB>
2023-11-18 15:27:45 +0100

    Add correct expected output

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,7 @@ function handleResponseFromAPI(promise){
   return promise
   .then((response) => {
     console.log("Got a response from the API");
-    return {status: 200, body: success};
+    return {status: 200, body: "success"};
   })
   .catch((error) => {
     console.log("Got a response from Api");
