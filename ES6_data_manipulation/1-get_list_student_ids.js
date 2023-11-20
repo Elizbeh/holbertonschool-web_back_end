77e4f3521ec5266baf373bb429ab0d3e2cba9b11
root <root@LAPTOP-J7HTNKBB>
2023-11-20 22:17:47 +0100

    npm run dev 1-main.js

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,12 @@
+function getListStudentIds(arrayOfStudents) {
+  if (!Array.isArray(arrayOfStudents)) {
+    return [];
+  }
+
+  // Use map function to extract ids from the array of objects
+  const ids = arrayOfStudents.map(student => student.id);
+
+  return ids;
+}
+
+export default getListStudentIds;
