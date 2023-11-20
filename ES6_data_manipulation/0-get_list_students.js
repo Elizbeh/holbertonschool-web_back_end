7ee2bbeb230f0f8014ac91216dd4117b92549667
root <root@LAPTOP-J7HTNKBB>
2023-11-20 16:24:36 +0100

    Eslint Error fixing

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,10 @@
-function getListStudents(id, firstName, location) {
+function getListStudents() {
   const studentList = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
+
   return studentList;
 }
 
