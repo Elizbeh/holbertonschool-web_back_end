55c4c2dde1d90d0d9b4f1367a5d599114e368f15
root <root@LAPTOP-J7HTNKBB>
2023-11-21 13:16:34 +0100

    7. More set data structure

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,7 @@
+function hasValuesFromArray(set, array) {
+
+  // Every method check if all elements in the array exist in the set
+  return array.every((value) => set.has(value));
+}
+
+export default hasValuesFromArray;
