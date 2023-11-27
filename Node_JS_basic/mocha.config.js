5cc76f394bb0ad52d53abb655b7c31fdc59d1bea
Elizbeh <eliz01_01@yahoo.com>
2023-11-28 00:10:01 +0100

    Add files

---
+++ Node_JS_basic/mocha.config.js
@@ -0,0 +1,8 @@
+// mocha.config.js
+module.exports = {
+  spec: './**/*.test.js', // Pattern to find test files
+};
+// mocha.config.js
+module.exports = {
+  spec: './**/*.test.js', // Pattern to find test files
+};
