035acb19f1f4693e447b597f79cb74758ba5e428
Elizbeh <eliz01_01@yahoo.com>
2023-11-28 00:17:42 +0100

    Add file

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,19 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const readline = require('readline');
 
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
-  }
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', () => {
+});
+
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
 });
