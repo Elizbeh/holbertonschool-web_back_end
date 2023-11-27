f08a37ea26baadea81c8a1f286e4be59eee3335b
Elizbeh <eliz01_01@yahoo.com>
2023-11-27 15:34:55 +0100

    Test

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // 0. Executing basic javascript with Node JS
-const displayMessage = (message) => {
-  console.log(message);
+const displayMessage = (str) => {
+  console.log(str);
 };
 
 module.exports = displayMessage;
