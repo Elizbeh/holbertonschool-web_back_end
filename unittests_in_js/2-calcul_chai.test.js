fb852b5219e0a53812b6c1206ea408a5b3d6719d
root <root@LAPTOP-J7HTNKBB>
2023-12-04 21:55:08 +0100

    2. Basic test using Chai assertion library

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,14 +1,13 @@
-const expect = require('chai').expect
+const expect = require('chai').expect;
 const calculateNumber = require('./2-calcul_chai');
 
-
 describe('calculateNumber', () => {
   it('should round and add two numbers when type is SUM', () => {
-    expect(calculateNumber('SUM', 1.4, 4.5).to.be.equal(6))
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6);
   });
 
   it('should round and subtract b from a when type is SUBTRACT', () => {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4); 
   });
 
   it('should round and divide a by b when type is DIVIDE', () => {
@@ -16,27 +15,7 @@ describe('calculateNumber', () => {
   });
 
   it('should return "Error: Division by zero is not allowed" when trying to divide by 0', () => {
-    expect(calculateNumber('DIVIDE', 1.4 , 0)).to.throw('Error: Division by zero is not allowed')
-  });
-
-  it('should round and add two positive integers when type is SUM', () => {
-    expect(calculateNumber('SUM', 3, 5)).to.equal(8);
-  });
-describe('calculateNumber', () => {
-  it('should round and add two numbers when type is SUM', () => {
-    expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6)
-  });
-
-  it('should round and subtract b from a when type is SUBTRACT', () => {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-  });
-
-  it('should round and divide a by b when type is DIVIDE', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2);
-  });
-
-  it('should return "Error: Division by zero is not allowed" when trying to divide by 0', () => {
-    expect(calculateNumber('DIVIDE', 1.4 , 0)).to.throw('Error: Division by zero is not allowed')
+    expect(() => calculateNumber('DIVIDE', 1.4, 0).to.throw('Error: Division by zero is not allowed'));
   });
 
   it('should round and add two positive integers when type is SUM', () => {
@@ -48,7 +27,7 @@ describe('calculateNumber', () => {
   });
 
   it('should round and divide a by b when type is DIVIDE for negative numbers', () => {
-    expect(calculateNumber('DIVIDE', -1.4, 4.5)).to.be.equal(-0.3);
+    expect(calculateNumber('DIVIDE', -1.4, 4.5)).to.be.equal(-0.2);
   });
 
   it('should round and add two decimal numbers when type is SUM', () => {
@@ -56,15 +35,15 @@ describe('calculateNumber', () => {
   });
 
   it('should round and subtract b from a when type is SUBTRACT for decimal numbers', () => {
-    expect(calculateNumber('SUBTRACT', 7.89, 2.34)).to.equal(5);
+    expect(calculateNumber('SUBTRACT', 7.89, 2.34)).to.equal(6);
   });
 
   it('should round and divide a by b when type is DIVIDE for decimal numbers', () => {
-    expect(calculateNumber('DIVIDE', 8.64, 2.16)).to.be.equal(4);
+    expect(calculateNumber('DIVIDE', 8.64, 2.16)).to.be.equal(4.5);
   });
 
   it('should return "Error: Division by zero is not allowed" when trying to divide by 0 for decimal numbers', () => {
-    expect(calculateNumber('DIVIDE', 5.5, 0)).to.throw('Error: Division by zero is not allowed');
+    expect(() => calculateNumber('DIVIDE', 5.5, 0).to.throw('Error: Division by zero is not allowed'));
   });
 
   it('should round and add two negative decimal numbers when type is SUM', () => {
@@ -72,26 +51,19 @@ describe('calculateNumber', () => {
   });
 
   it('should round and subtract b from a when type is SUBTRACT for positive decimal numbers', () => {
-    expect(calculateNumber('SUBTRACT', 7.89, 2.34)).to.be.equal(5);
+    expect(calculateNumber('SUBTRACT', 7.89, 2.34)).to.be.equal(6);
   });
 
   it('should round and divide a by b when type is DIVIDE for positive decimal numbers', () => {
-    expect(calculateNumber('DIVIDE', 8.64, 2.16)).to.be.equal(4);
+    expect(calculateNumber('DIVIDE', 8.64, 2.16)).to.be.equal(4.5);
   });
 
   it('should return "Error: Division by zero is not allowed" when trying to divide by 0 for negative decimal numbers', () => {
-    expect(calculateNumber('DIVIDE', -5.5, 0)).to.throw('Error: Division by zero is not allowed');
-  });
-
-  it('should round and add two zero values when type is SUM', () => {
-    expect(calculateNumber('SUM', 0, 0)).to.be.equal(0);
-  });
-
-  it('should round and subtract b from a when type is SUBTRACT for zero values', () => {
-    expect(calculateNumber('SUBTRACT', 0, 0)).to.be.equal(0);
+    expect(() => calculateNumber('DIVIDE', -5.5, 0).to.throw('Error: Division by zero is not allowed'));
   });
 
   it('should return "Error: Division by zero is not allowed" when trying to divide by 0 for zero values', () => {
-    expect(() => calculateNumber('DIVIDE', 0, 0)).to.throw('Error: Division by zero is not allowed');
+    expect(() => calculateNumber('DIVIDE', 0, 0).to.throw('Error: Division by zero is not allowed'));
   });
 });
+
