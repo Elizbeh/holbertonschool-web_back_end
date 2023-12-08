3b5dc94d8dbf96ea22b34dae8fb53bb5ab6c59d5
root <root@LAPTOP-J7HTNKBB>
2023-12-08 14:22:54 +0100

    Error handling elaborated

--- queuing_system_in_js/4-redis_advanced_op.js
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -8,15 +8,55 @@ client.on('connect', () => {
     console.log('Redis client connected to the server');
 });
 
-client.hset('HolbertonSchools', 'Portland', 50, redis.print);
-client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
-client.hset('HolbertonSchools', 'New York', 20, redis.print);
-client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
-client.hset('HolbertonSchools', 'Cali', 40, redis.print);
-client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+client.hset('HolbertonSchools', 'Portland', 50, (err, reply) => {
+    if (err) {
+        console.error(`Error setting Portland: ${err}`);
+    } else {
+        console.log(`Reply: ${reply}`);
+    }
+});
+
+client.hset('HolbertonSchools', 'Seatle', 80, (err, reply) => {
+    if (err) {
+        console.error(`Error setting Seatle: ${err}`);
+    } else {
+        console.log(`Reply: ${reply}`);
+    }
+});
+
+client.hset('HolbertonSchools', 'New York', 20, (err, reply) => {
+    if (err) {
+        console.error(`Error setting Portland: ${err}`);
+    } else {
+        console.log(`Reply: ${reply}`);
+    }
+});
+
+client.hset('HolbertonSchools', 'Bogota', 20, (err, reply) => {
+    if (err) {
+        console.error(`Error setting Bogota: ${err}`);
+    } else {
+        console.log(`Reply: ${reply}`);
+    }
+});
+
+client.hset('HolbertonSchools', 'Cali', 40, (err, reply) => {
+    if (err) {
+        console.error(`Error setting Cali: ${err}`);
+    } else {
+        console.log(`Reply: ${reply}`);
+    }
+});
+
+client.hset('HolbertonSchools', 'Paris', 2, (err, reply) => {
+    if (err) {
+        console.error(`Error setting Paris: ${err}`);
+    } else {
+        console.log(`Reply: ${reply}`);
+    }
+});
 
 client.hgetall('HolbertonSchools', (err, reply) => {
     if (err) throw err;
     console.log(reply);
 });
-
